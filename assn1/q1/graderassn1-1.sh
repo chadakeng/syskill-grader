b5941faffc26872f552465fb44af189168296ea8
chadakeng <chada.keng@gmail.com>
2023-01-28 11:27:54 +0700

    general case done

--- assn1/q1/graderassn1-1.sh
+++ assn1/q1/graderassn1-1.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-# generate files
-mkdir small
-mkdir medium
-mkdir large
+# variables
+name=( "Low" "Medium" "High" )
+exp=0
+score=0
 
-dd 
+# create files with desired sizes
 
-rm -r small
-rm -r medium
-rm -r large
+for i in ${name[@]}; do
+    mkdir $i
+    dd if=/dev/zero of=./$i/output.dat status=none  bs=500k  count=$((2 ** exp))
+
+    temp=$(./quota.sh "$i")
+
+    if [ "$temp" == "$i" ]; then
+        ((score=score+10))
+        echo $i: Passed
+    else
+        echo $i: Failed
+    fi
+    ((exp=exp+1))
+    rm -r $i
+done
+
+echo --------------------------
+echo "Score: $score/30"
+echo --------------------------
