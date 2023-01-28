33823bbf583c86e91eaccad83511c876d271f0bf
chadakeng <chada.keng@gmail.com>
2023-01-28 15:15:55 +0700

    can now check functionality of textfile creation. Output student's out if fail test

--- assn1/q1/graderassn1-1.sh
+++ assn1/q1/graderassn1-1.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
 
+# ---- NOTES -----
+# couldnt figure out how to calculate decimal points so leaving it as kB x)
+
 # variables
 name=( "Low" "Medium" "Medium" "High" "High" )
 mult=1
 score=0
 
 for i in ${name[@]}; do
-    mkdir temp
+    mkdir -p ./$mult/subfolder/subsubfolder
 
     # create files with desired sizes
-    dd if=/dev/zero of=./temp/output.dat status=none  bs=500k  count=$mult
+    dd if=/dev/zero of=./$mult/subfolder/subsubfolder/output.dat status=none  bs=500k  count=$mult
 
-    temp=$(./quota.sh "temp")
+    # run student's script
+    temp=$(./quota.sh ./$mult/subfolder/subsubfolder)
+
     ((size_pr=500*mult))
+
     if [ "$temp" == "$i" ]; then
         ((score=score+10))
         echo $i "$size_pr"kB: Passed
     else
-        # check if wrong formatting before failing test for partial
+        # check if wrong formatting before failing test for partial credit
         lowered=$(echo $temp | awk '{print tolower($0)}')
         size=$(echo $i | awk '{print tolower($0)}')
+
         if [ "$lowered" == "$size" ]; then
-            echo "$i $size_pr"kB: Wrong format, right output.
             ((score=score+5))
+            echo "$i $size_pr"kB: Wrong format, right output.
         else
             echo $i "$size_pr"kB: Failed
+            echo "  ST: $temp"
+            echo "  EO: $i"
         fi
     fi
+    rm -r ./$mult
     ((mult=mult+1))
-    rm -r temp
 done
 
+echo 
+if cmp -s ~/ListOfBigDirs.txt ./eo1-1.txt; then
+    ((score=score+10))
+    echo ListOfBigDirs.txt: Passed
+else
+    echo ListOfBigDirs.txt: Failed
+    echo "  ST":
+    cat ~/ListOfBigDirs.txt
+    echo "  EO":
+    cat ./eo1-1.txt
+fi
+
 echo --------------------------
-echo "Score: $score/50"
+echo "Score: $score/60"
 echo --------------------------
+rm ~/ListOfBigDirs.txt
