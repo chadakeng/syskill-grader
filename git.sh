272cb2a332f85ff5017b1f0944b68c5d703d167c
chadakeng <chada.keng@gmail.com>
2023-02-03 13:07:46 +0700

    Delete git.sh

--- git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git log --pretty=format:"[%cN]: %cd" --date=format-local:'%d %B %Y'
-
-# References
-# https://stackoverflow.com/questions/7853332/how-to-change-git-log-date-formats
-# https://git-scm.com/docs/git-log
